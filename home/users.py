9d00198b55748057550f6cb4ef2ece39d4f8407f
daniiarss <daniarsovetv007@gmail.com>
2024-05-25 23:36:41 +0600

    ааа

---
+++ home/users.py
@@ -0,0 +1,6 @@
+from bank import BigBank
+bank_instance = BigBank("My Bank   ", 1000)
+bank_instance.name = "New Bank Name   "
+bank_instance.balance = 2000
+print(bank_instance.name)
+print(bank_instance.balance)
